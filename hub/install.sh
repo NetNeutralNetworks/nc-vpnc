683c5fc4d39d6667d756c75f95306e09d4b7db86
Tony Fortes Ramos <amfortesramos@hotmail.com>
2022-11-15 07:42:18 +0000

    Update installation script

--- hub/install.sh
+++ hub/install.sh
@@ -2,21 +2,39 @@
 SCRIPTDIR="$(dirname -- "$BASH_SOURCE")"
 VENVDIR=/opt/ncubed/vpnc.service/.venv
 
-# add FRR GPG key
-curl -s https://deb.frrouting.org/frr/keys.asc | sudo apt-key add -
+case $1 in
+hub)
+    # add FRR GPG key
+    curl -s https://deb.frrouting.org/frr/keys.asc | sudo apt-key add -
 
-# possible values for FRRVER: frr-6 frr-7 frr-8 frr-stable
-# frr-stable will be the latest official stable release
-FRRVER="frr-8"
-echo deb https://deb.frrouting.org/frr $(lsb_release -s -c) $FRRVER | sudo tee /etc/apt/sources.list.d/frr.list
+    # possible values for FRRVER: frr-6 frr-7 frr-8 frr-stable
+    # frr-stable will be the latest official stable release
+    FRRVER="frr-8"
+    echo deb https://deb.frrouting.org/frr $(lsb_release -s -c) $FRRVER | sudo tee /etc/apt/sources.list.d/frr.list
 
-# update and install FRR/strongSwan
-apt update
-apt install -y python3-venv python3-pip strongswan strongswan-swanctl jool-tools frr frr-pythontools frr-snmp
+    # update and install FRR/strongSwan
+    apt update
+    apt install -y python3-venv python3-pip strongswan strongswan-swanctl jool-tools frr frr-pythontools frr-snmp
 
-cp -rf $SCRIPTDIR/etc/* /etc/
-cp -rf $SCRIPTDIR/opt/* /opt/
-python3 -m venv $VENVDIR
-$VENVDIR/bin/python3 -m pip install -r $SCRIPTDIR/requirements.txt
+    cp -rf $SCRIPTDIR/etc/* /etc/
+    cp -rf $SCRIPTDIR/opt/* /opt/
+    python3 -m venv $VENVDIR
+    $VENVDIR/bin/python3 -m pip install -r $SCRIPTDIR/requirements.txt
 
-/opt/ncubed/vpnc.service/scripts/install.sh
+    /opt/ncubed/vpnc.service/scripts/install.sh hub
+    ;;
+endpoint)
+    # update and install strongSwan
+    apt update
+    apt install -y python3-venv python3-pip strongswan strongswan-swanctl
+
+    cp -rf $SCRIPTDIR/etc/* /etc/
+    cp -rf $SCRIPTDIR/opt/* /opt/
+    python3 -m venv $VENVDIR
+    $VENVDIR/bin/python3 -m pip install -r $SCRIPTDIR/requirements.txt
+
+    /opt/ncubed/vpnc.service/scripts/install.sh endpoint
+    ;;
+*)
+    echo "Argument should be either 'hub' or 'endpoint'"
+esac
--- hub/opt/ncubed/vpnc.service/scripts/install.sh
+++ hub/opt/ncubed/vpnc.service/scripts/install.sh
@@ -1,25 +1,45 @@
 #! /bin/bash
 servicename=vpnc
-servicefile=/etc/systemd/system/ncubed-$servicename.service
-if [ -L $servicefile ]; then
-  rm $servicefile
-fi
-ln -s /opt/ncubed/$servicename.service/units/ncubed-$servicename.service $servicefile
+case $1 in
+hub)
+    /usr/bin/systemctl disable ncubed-$servicename-hub.service
+    /usr/bin/systemctl stop ncubed-$servicename-endpoint.service
+    /usr/bin/systemctl disable ncubed-$servicename-endpoint.service
 
-# Enable bgpd in FRR
-sed -i 's/^bgpd=no$/bgpd=yes/' /etc/frr/daemons
-sed -i 's/^zebra_options="  -A 127.0.0.1 -s 90000000.*"$/zebra_options="  -A 127.0.0.1 -s 90000000 -n -M snmp"/' /etc/frr/daemons
-sed -i 's/^bgpd_options="   -A 127.0.0.1.*"$/bgpd_options="   -A 127.0.0.1 -M snmp"/' /etc/frr/daemons
+    /usr/bin/systemctl link /opt/ncubed/$servicename.service/units/ncubed-$servicename-hub.service
 
-# comment SNMP agentaddress in snmpd
-sed -i -E 's/^agentaddress(.*)/#agentaddress\1/' /etc/snmp/snmpd.conf
+    # Enable bgpd in FRR
+    sed -i 's/^bgpd=no$/bgpd=yes/' /etc/frr/daemons
+    sed -i 's/^zebra_options="  -A 127.0.0.1 -s 90000000.*"$/zebra_options="  -A 127.0.0.1 -s 90000000 -n -M snmp"/' /etc/frr/daemons
+    sed -i 's/^bgpd_options="   -A 127.0.0.1.*"$/bgpd_options="   -A 127.0.0.1 -M snmp"/' /etc/frr/daemons
 
-/usr/bin/systemctl daemon-reload
-/usr/bin/systemctl disable ipsec.service
-/usr/bin/systemctl stop ipsec.service
-/usr/bin/systemctl enable snmpd.service
-/usr/bin/systemctl restart snmpd.service
-/usr/bin/systemctl enable frr.service
-/usr/bin/systemctl restart frr.service
-/usr/bin/systemctl enable ncubed-$servicename
-/usr/bin/systemctl restart ncubed-$servicename
+    # comment SNMP agentaddress in snmpd
+    sed -i -E 's/^agentaddress(.*)/#agentaddress\1/' /etc/snmp/snmpd.conf
+
+    /usr/bin/systemctl daemon-reload
+    /usr/bin/systemctl disable ipsec.service
+    /usr/bin/systemctl stop ipsec.service
+    /usr/bin/systemctl enable snmpd.service
+    /usr/bin/systemctl restart snmpd.service
+    /usr/bin/systemctl enable frr.service
+    /usr/bin/systemctl restart frr.service
+    /usr/bin/systemctl enable ncubed-$servicename-hub
+    /usr/bin/systemctl restart ncubed-$servicename-hub
+    ;;
+endpoint)
+    # It's important to have the link have the same name as the desired service, otherwise the symlink won't work.
+    /usr/bin/systemctl stop ncubed-$servicename-hub.service
+    /usr/bin/systemctl disable ncubed-$servicename-hub.service
+    /usr/bin/systemctl disable ncubed-$servicename-endpoint.service
+
+    /usr/bin/systemctl link /opt/ncubed/$servicename.service/units/ncubed-$servicename-endpoint.service
+
+    /usr/bin/systemctl daemon-reload
+    /usr/bin/systemctl disable ipsec.service
+    /usr/bin/systemctl stop ipsec.service
+    /usr/bin/systemctl enable ncubed-$servicename-endpoint
+    /usr/bin/systemctl restart ncubed-$servicename-endpoint
+    ;;
+*)
+    echo "Argument should be either 'hub' or 'endpoint'"
+esac
